fbdf30c5a96360197b4d56512109215b6b81ef48
Fernando Neira <fe.neiraj@gmail.com>
2024-02-05 21:02:10 -0300

    interface-processor: add BeanFactory

--- interfaceprocessor/src/main/java/dev/fneira/interfaceprocessor/imp/annotations/ProxyHandler.java
+++ interfaceprocessor/src/main/java/dev/fneira/interfaceprocessor/imp/annotations/ProxyHandler.java
@@ -1,5 +1,7 @@
 package dev.fneira.interfaceprocessor.imp.annotations;
 
+import org.springframework.cglib.proxy.MethodInterceptor;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -8,5 +10,5 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ProxyHandler{
-    Class<?> value();
+    Class<? extends MethodInterceptor> value();
 }
